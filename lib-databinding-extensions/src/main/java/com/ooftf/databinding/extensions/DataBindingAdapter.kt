8ced0c3255456e500e48cc891e36b4413c6c69a3
ooftf <li994749769@gmail.com>
2019-12-27 15:28:35 +0800

    data binding 0.0.4

--- lib-databinding-extensions/src/main/java/com/ooftf/databinding/extensions/DataBindingAdapter.kt
+++ lib-databinding-extensions/src/main/java/com/ooftf/databinding/extensions/DataBindingAdapter.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -58,6 +59,15 @@ object DataBindingAdapter {
             imageView.setImageDrawable(null)
         } else {
             imageView.setImageResource(resource)
+        }
+
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["exTextColorId"])
+    fun setTextColorResource(textView: TextView, resource: Int?) {
+        if (resource != null && resource != 0&& resource!=View.NO_ID) {
+            textView.setTextColor(ContextCompat.getColor(textView.context,resource))
         }
 
     }
