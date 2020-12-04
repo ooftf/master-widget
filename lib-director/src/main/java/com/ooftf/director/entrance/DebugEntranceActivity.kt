f530e79e58f576524b558bc3ff965fc33984432d
ooftf <li994749769@gmail.com>
2020-12-04 11:41:07 +0800

    SYNC

--- lib-director/src/main/java/com/ooftf/director/entrance/DebugEntranceActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.ooftf.director.entrance
-
-import com.ooftf.arch.frame.mvvm.activity.BaseMvvmActivity
-import com.ooftf.director.databinding.DirectorOoftfActivityCommonListBinding
-
-class DebugEntranceActivity : BaseMvvmActivity<DirectorOoftfActivityCommonListBinding, DebugEntranceViewModel>() {
-}
