fcfdbf32646cccd7da8138bb03d66551a393e328
ooftf <li994749769@gmail.com>
2021-01-06 15:52:30 +0800

    dataBinding-extension 1.1.5

--- lib-databinding-extensions/src/main/java/com/ooftf/databinding/extensions/empty/OnListChangedCallbackPoly.kt
+++ lib-databinding-extensions/src/main/java/com/ooftf/databinding/extensions/empty/OnListChangedCallbackPoly.kt
@@ -8,12 +8,12 @@ import androidx.databinding.ObservableList
  * @email 994749769@qq.com
  * @date 2020/11/6
  */
-open class OnListChangedCallbackPoly<T : ObservableList<*>>(var action: ((T) -> Unit)?,
-                                                            var onChanged: ((T) -> Unit)?,
-                                                            var onItemRangeChanged: ((sender: T, positionStart: Int, itemCount: Int) -> Unit)?,
-                                                            var onItemRangeInserted: ((sender: T, positionStart: Int, itemCount: Int) -> Unit)?,
-                                                            var onItemRangeMoved: ((sender: T, fromPosition: Int, toPosition: Int, itemCount: Int) -> Unit)?,
-                                                            var onItemRangeRemoved: ((sender: T, positionStart: Int, itemCount: Int) -> Unit)?) : ObservableList.OnListChangedCallback<T>() {
+open class OnListChangedCallbackPoly<T : ObservableList<*>>(var action: ((T) -> Unit)? = null,
+                                                            var onChanged: ((T) -> Unit)? = null,
+                                                            var onItemRangeChanged: ((sender: T, positionStart: Int, itemCount: Int) -> Unit)? = null,
+                                                            var onItemRangeInserted: ((sender: T, positionStart: Int, itemCount: Int) -> Unit)? = null,
+                                                            var onItemRangeMoved: ((sender: T, fromPosition: Int, toPosition: Int, itemCount: Int) -> Unit)? = null,
+                                                            var onItemRangeRemoved: ((sender: T, positionStart: Int, itemCount: Int) -> Unit)? = null) : ObservableList.OnListChangedCallback<T>() {
 
     override fun onChanged(sender: T) {
         action?.invoke(sender)
