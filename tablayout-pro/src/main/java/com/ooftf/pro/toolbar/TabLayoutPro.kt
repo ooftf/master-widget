0f753346a90d2600695f0699877d4d23c0b26694
ooftf <li994749769@gmail.com>
2021-06-29 15:08:40 +0800

    优化 TabLayoutPro 代码结构

--- tablayout-pro/src/main/java/com/ooftf/pro/toolbar/TabLayoutPro.kt
+++ tablayout-pro/src/main/java/com/ooftf/pro/toolbar/TabLayoutPro.kt
@@ -2,12 +2,9 @@ package com.ooftf.pro.toolbar
 
 import android.content.Context
 import android.graphics.Typeface
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.LayerDrawable
 import android.util.AttributeSet
 import android.widget.TextView
 import com.google.android.material.tabs.TabLayout
-import com.ooftf.basic.utils.DensityUtil
 import com.ooftf.basic.utils.reflectGetField
 
 /**
@@ -17,28 +14,8 @@ import com.ooftf.basic.utils.reflectGetField
  * @date 2021/1/6
  */
 open class TabLayoutPro : TabLayout {
-
-    constructor(context: Context) : super(context) {
-
-    }
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        obtainStyle(attrs)
-
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        obtainStyle(attrs)
-
-    }
-
-
-    fun getTabText(tab: Tab?): TextView? {
-        return (tab?.view?.reflectGetField("textView") as? TextView)
-    }
-
-    private fun boldChange() {
-        addOnTabSelectedListener(object : OnTabSelectedListener {
+    val listener by lazy {
+        object : OnTabSelectedListener {
             override fun onTabReselected(tab: Tab?) {
 
             }
@@ -55,36 +32,104 @@ open class TabLayoutPro : TabLayout {
                 }
             }
 
-        })
+        }
     }
 
+    constructor(context: Context) : super(context) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        attrs?.let { obtainStyle(it) }
+
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        attrs?.let { obtainStyle(it) }
+    }
+
+
+    fun getTabText(tab: Tab?): TextView? {
+        return (tab?.view?.reflectGetField("textView") as? TextView)
+    }
+
+    fun boldChange(change: Boolean) {
+        if (change) {
+            addOnTabSelectedListener(listener)
+        } else {
+            removeOnTabSelectedListener(listener)
+        }
+    }
+
+    fun setIndicatorHeight(height: Int) {
+        var indicator = tabSelectedIndicator as? IndicatorDrawable
+        if (indicator == null) {
+            indicator = IndicatorDrawable()
+            setSelectedTabIndicator(indicator)
+        }
+        indicator.height = height.toFloat()
+    }
+
+    fun setIndicatorWidth(width: Int) {
+        var indicator = tabSelectedIndicator as? IndicatorDrawable
+        if (indicator == null) {
+            indicator = IndicatorDrawable()
+            setSelectedTabIndicator(indicator)
+        }
+        indicator.width = width.toFloat()
+    }
+
+    fun setIndicatorRadius(radius: Int) {
+        var indicator = tabSelectedIndicator as? IndicatorDrawable
+        if (indicator == null) {
+            indicator = IndicatorDrawable()
+            setSelectedTabIndicator(indicator)
+        }
+        indicator.cornerRadius = radius.toFloat()
+    }
 
     fun obtainStyle(attrs: AttributeSet) {
         val tabLayoutStyle = context.obtainStyledAttributes(attrs, R.styleable.TabLayout)
         val tabLayoutProStyle = context.obtainStyledAttributes(attrs, R.styleable.TabLayoutPro)
         if (tabLayoutProStyle.hasValue(R.styleable.TabLayoutPro_tabIndicatorWidth)) {
-            val indicatorDrawable = IndicatorDrawable()
-            setSelectedTabIndicator(IndicatorDrawable())
             if (tabLayoutStyle.hasValue(com.google.android.material.R.styleable.TabLayout_tabIndicatorHeight)) {
-                indicatorDrawable.width = tabLayoutProStyle.getDimensionPixelSize(R.styleable.TabLayoutPro_tabIndicatorWidth, -1)
-                    .toFloat()
-                indicatorDrawable.height =
-                    tabLayoutStyle.getDimensionPixelSize(R.styleable.TabLayout_tabIndicatorHeight, -1)
-                        .toFloat()
-            } else {
-                if (tabLayoutStyle.hasValue(R.styleable.TabLayoutPro_tabIndicatorWidth)) {
-                    indicatorDrawable.width = tabLayoutProStyle.getDimensionPixelSize(R.styleable.TabLayoutPro_tabIndicatorWidth, -1).toFloat()
-                }
+                setIndicatorHeight(
+                    tabLayoutStyle.getDimensionPixelSize(
+                        R.styleable.TabLayout_tabIndicatorHeight,
+                        -1
+                    )
+                )
+            }
+            if (tabLayoutStyle.hasValue(R.styleable.TabLayoutPro_tabIndicatorWidth)) {
+                setIndicatorWidth(
+                    tabLayoutProStyle.getDimensionPixelSize(
+                        R.styleable.TabLayoutPro_tabIndicatorWidth,
+                        -1
+                    )
+                )
             }
 
             if (tabLayoutProStyle.hasValue(R.styleable.TabLayoutPro_tabIndicatorRadius)) {
-                indicatorDrawable.cornerRadius = tabLayoutProStyle.getDimensionPixelSize(R.styleable.TabLayoutPro_tabIndicatorRadius, -1).toFloat()
+                setIndicatorRadius(
+                    tabLayoutProStyle.getDimensionPixelSize(
+                        R.styleable.TabLayoutPro_tabIndicatorRadius,
+                        -1
+                    )
+                )
             }
         }
         if (tabLayoutProStyle.hasValue(R.styleable.TabLayoutPro_tabTextBoldChange)) {
-            if (tabLayoutProStyle.getBoolean(R.styleable.TabLayoutPro_tabTextBoldChange, false)) {
-                boldChange()
-            }
+            boldChange(
+                tabLayoutProStyle.getBoolean(
+                    R.styleable.TabLayoutPro_tabTextBoldChange,
+                    false
+                )
+            )
         }
 
         tabLayoutStyle.recycle()
