c8233055e3440939d51138bd4bc9314b1520fcfd
hanyi <hanyi.lh@alibaba-inc.com>
2022-04-07 15:02:39 +0800

    优化高斯模糊代码

--- lib-dialog/src/main/java/com/ooftf/master/widget/dialog/utils/BitmapUtils.java
+++ lib-dialog/src/main/java/com/ooftf/master/widget/dialog/utils/BitmapUtils.java
@@ -7,6 +7,8 @@ import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
 
+import androidx.annotation.Nullable;
+
 /**
  * @author ooftf
  * @email 994749769@qq.com
@@ -21,49 +23,53 @@ public class BitmapUtils {
      * @param radius
      * @return
      */
-    public static Bitmap rsBlur(Context context, Bitmap source, double radius) {
+    public static @Nullable Bitmap rsBlur(Context context, Bitmap source, double radius) {
         Bitmap inputBmp;
-        if(radius >25){
-            double scale = radius/25.0;
+        if (radius > 25) {
+            double scale = radius / 25;
             radius = 25;
-            int scaleWith = Math.max((int)(source.getWidth()/scale),1);
-            int scaleHeight = Math.max((int)(source.getHeight()/scale),1);
-            inputBmp = Bitmap.createScaledBitmap(source,scaleWith,scaleHeight,false);
-        }else {
+            int scaleWith = Math.max((int) (source.getWidth() / scale), 1);
+            int scaleHeight = Math.max((int) (source.getHeight() / scale), 1);
+            inputBmp = Bitmap.createScaledBitmap(source, scaleWith, scaleHeight, false);
+        } else {
             inputBmp = source;
         }
+        Bitmap outBitmap = null;
+        Allocation input = null;
+        Allocation output = null;
+        RenderScript renderScript = null;
+        ScriptIntrinsicBlur scriptIntrinsicBlur = null;
+        try {
+            renderScript = RenderScript.create(context);
+            input = Allocation.createFromBitmap(renderScript, inputBmp);
+            output = Allocation.createTyped(renderScript, input.getType());
+            scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+            scriptIntrinsicBlur.setInput(input);
+            scriptIntrinsicBlur.setRadius((float) radius);
+            scriptIntrinsicBlur.forEach(output);
+            if (inputBmp == source) {
+                outBitmap = Bitmap.createBitmap(inputBmp.getWidth(), inputBmp.getHeight(), inputBmp.getConfig());
+            } else {
+                outBitmap = inputBmp;
+            }
 
-        //(1)
-        RenderScript renderScript = RenderScript.create(context);
-
-        // Allocate memory for Renderscript to work with
-        //(2)
-        final Allocation input = Allocation.createFromBitmap(renderScript, inputBmp);
-        final Allocation output = Allocation.createTyped(renderScript, input.getType());
-        //(3)
-        // Load up an instance of the specific script that we want to use.
-        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
-        //(4)
-        scriptIntrinsicBlur.setInput(input);
-        //(5)
-        // Set the blur radius
-        scriptIntrinsicBlur.setRadius((float) radius);
-        //(6)
-        // Start the ScriptIntrinisicBlur
-        scriptIntrinsicBlur.forEach(output);
-        //(7)
-        // Copy the output to the blurred bitmap
-        Bitmap outBitmap;
-        if(inputBmp == source){
-            outBitmap = Bitmap.createBitmap(inputBmp.getWidth(),inputBmp.getHeight(),inputBmp.getConfig());
-        }else{
-            outBitmap = inputBmp;
+            output.copyTo(outBitmap);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            if (renderScript != null) {
+                renderScript.destroy();
+            }
+            if (output != null) {
+                output.destroy();
+            }
+            if (input != null) {
+                input.destroy();
+            }
+            if (scriptIntrinsicBlur != null) {
+                scriptIntrinsicBlur.destroy();
+            }
         }
-
-        output.copyTo(outBitmap);
-
-        //(8)
-        renderScript.destroy();
         return outBitmap;
     }
 }
